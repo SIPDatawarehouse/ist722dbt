ea57c2b36b52491cade63e2b7788446c2d393cd9
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-11-06 15:16:08 +0000

    1st business process

---
+++ quickstart/models/dim_titles.sql
@@ -0,0 +1,12 @@
+with stg_titles as (
+    select * from {{ source('pubs','Titles')}}
+)
+select  {{ dbt_utils.generate_surrogate_key(['stg_titles.title_id']) }} as titlekey, 
+    stg_titles.title_id as titleid,
+    stg_titles.title as titlename,
+    stg_titles.type as titletype,
+    stg_titles.price as titleprice,
+    stg_titles.notes as titlenotes,
+    stg_titles.pubdate as titlepubdate,
+    stg_titles.pub_id as pub_id
+from stg_titles
