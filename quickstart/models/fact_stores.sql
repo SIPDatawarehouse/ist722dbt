15d9069565bc7063a2dde082cf68e2e01e962a21
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-11-27 01:08:23 +0000

    Commit and Sync

--- quickstart/models/fact_stores.sql
+++ quickstart/models/fact_stores.sql
@@ -6,19 +6,21 @@ with
             stor_id,
             {{ dbt_utils.generate_surrogate_key(["title_id"]) }} as titlekey,
             {{ dbt_utils.generate_surrogate_key(["stor_id"]) }} as storekey,
-            qty as quantity
+            qty as quantity,
+            case when stor_id = 8042 then 5 else 0 end as discount
         from {{ source("pubs", "Sales") }}
     ),
-    stg_title as (select title_id, price from {{ source("pubs", "Titles") }}),
-    stg_discounts as (select * from {{ source("pubs", "Discounts") }}),
-    stg_stores as (select * from {{ source("pubs", "Stores") }})
+    stg_title as (select title_id, price, pub_id from {{ source("pubs", "Titles") }}),  -- Added pub_id here
+    stg_publishers as (select * from {{ source("pubs", "Publishers") }})
 
 select
-    s.*,
-    case when d.discount is not null then d.discount else 0 end as discount,
+    s.orderid,
+    s.titlekey,
+    s.storekey,
+    s.quantity,
     s.quantity * t.price as income,
-    s.quantity * (t.price * (1 - (d.discount / 100))) as discountedincome
-from stg_sales s
-join stg_discounts d on s.stor_id = d.stor_id
-join stg_title t on t.title_id = s.title_id
-join stg_stores so on so.stor_id = s.stor_id
+    s.quantity * (t.price * (1 - (s.discount / 100))) as discountedincome
+from stg_title t
+join stg_publishers p on t.pub_id = p.pub_id
+join stg_sales s on t.title_id = s.title_id
+
