e547dda1e6f0fa8f1d5312ce1db29b9c26ca302e
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-12-01 07:11:56 +0000

    Commit and Sync

--- quickstart/models/dim_titles.sql
+++ quickstart/models/dim_titles.sql
@@ -1,7 +1,6 @@
-with stg_titles as (
-    select * from {{ source('pubs','Titles')}}
-)
-select  {{ dbt_utils.generate_surrogate_key(['stg_titles.title_id']) }} as titlekey, 
+with stg_titles as (select * from {{ source("pubs", "Titles") }})
+select
+    {{ dbt_utils.generate_surrogate_key(["stg_titles.title_id"]) }} as titlekey,
     stg_titles.title_id as titleid,
     stg_titles.title as titlename,
     stg_titles.type as titletype,
