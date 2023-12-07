780dd2ddd9f067752ad9db33e22040647404931c
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-12-07 23:58:17 +0000

    Commit and Sync

--- quickstart/models/fact_title_author.sql
+++ quickstart/models/fact_title_author.sql
@@ -6,7 +6,7 @@ with
             *
         from {{ source("pubs", "Titleauthor") }}
     ),
-    stg_title as (
+    stg_titles as (
         select
             {{ dbt_utils.generate_surrogate_key(["title_id"]) }} as titlekey,
             {{ dbt_utils.generate_surrogate_key(["pub_id"]) }} as publisherskey,
@@ -28,4 +28,4 @@ select
     ((a.royaltyper / 100) * t.price * t.ytd_sales) as effectiveroyaltyearned_row,
     ((t.royalty * t.ytd_sales / 100) - t.advance) as netearnings_row
 from stg_title_author a
-join stg_title t on a.titlekey = t.titlekey
+join stg_titles t on a.titlekey = t.titlekey
