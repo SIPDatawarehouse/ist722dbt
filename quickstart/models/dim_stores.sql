66e34d5cbef738eaf09362e12205863af13e0878
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-12-07 23:42:11 +0000

    Commit and Sync

--- quickstart/models/dim_stores.sql
+++ quickstart/models/dim_stores.sql
@@ -1,7 +1,6 @@
-with stg_stores as (
-    select * from {{ source('pubs','Stores')}}
-)
-select  {{ dbt_utils.generate_surrogate_key(['stg_stores.stor_id']) }} as storekey, 
+with stg_stores as (select * from {{ source("pubs", "Stores") }})
+select
+    {{ dbt_utils.generate_surrogate_key(["stg_stores.stor_id"]) }} as storekey,
     stg_stores.stor_id as storeid,
     stg_stores.stor_name as storename,
     stg_stores.stor_address as storeaddress,
--- quickstart/models/test.sql
+++
@@ -1,1 +0,0 @@
-select * from {{ source("pubs", "Titleauthor") }} where title_id = 'BU1032'
