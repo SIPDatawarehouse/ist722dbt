f21fbaaf338c6712321ea418ccb80a5dcdd0ed11
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-12-09 08:08:21 +0000

    Commit and Sync

--- quickstart/models/obt_title_author.sql
+++ quickstart/models/obt_title_author.sql
@@ -1,6 +1,6 @@
 with
     f_title_author as (select * from {{ ref("fact_title_author") }}),
-    d_authors as (select * from {{ ref("dim_authors") }}),
+    d_authors as (select * from {{ ref('dim_authors') }}),
     d_date as (select * from {{ ref("dim_date") }}),
     d_publishers as (select * from {{ ref("dim_publishers") }}),
     d_title as (select * from {{ ref("dim_titles") }})
@@ -13,7 +13,7 @@ select
     f.effectiveroyaltyearned,
     f.netearnings
 from f_title_author f
-left join d_title d on d.titlekey = f.titlekey
+left join d_title d on f.titlekey = d.titlekey
 left join d_authors a on f.authorkey = a.authorkey
 left join d_publishers p on f.publisherskey = p.publisherskey
 left join d_date on f.pubdatekey = d_date.datekey
