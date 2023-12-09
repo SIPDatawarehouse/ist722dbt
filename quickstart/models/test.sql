1cf5067c5d3fb5fda0359a3bc5ab64dbc06c1218
SIP Datawarehouse <SIPDatawarehouse@gmail.com>
2023-12-09 07:40:21 +0000

    Commit and Sync

--- quickstart/models/test.sql
+++
@@ -1,9 +0,0 @@
-with
-    f_title_author as (select * from {{ ref("fact_title_author") }}),
-    d_authors as (select * from {{ ref("dim_authors") }}),
-    d_date as (select * from {{ ref("dim_date") }})
-
-select *
-from f_title_author f
-left join d_authors a on f.authorkey = a.authorkey
-left join d_date d on f.pubdatekey = d.datekey
